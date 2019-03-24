a290076edc2189a500d0503487209d0b5ca462df
Roman <s210240@users.noreply.github.com>
2019-03-24 14:33:34 +0200

    chanels and blade

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,11 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('order.{orderId}', function ($user, $orderId) {
+    return $user->id === 1;//Order::findOrNew($orderId)->user_id;
+});
+
+Broadcast::channel('pets.{Id}', function ($user, $orderId) {
+    return $user->id === 1;//Order::findOrNew($orderId)->user_id;
+});
